2bf373d401244b8f81bc5a45e82ca399d51f5cf9
Tanner Richards <Tanner.richards990@gmail.com>
2022-10-04 12:16:54 -0700

    built out the box js and return

---
+++ src/Components/Box/Box.js
@@ -0,0 +1,12 @@
+import { useContext } from 'react';
+import { GameContext } from '../../context/GameContext';
+
+export default function Box({ space, content }) {
+  const { updateBoard } = useContext(GameContext);
+
+  return (
+    <div className="box" onClick={() => updateBoard(space)} id={space}>
+      {content}
+    </div>
+  );
+}
